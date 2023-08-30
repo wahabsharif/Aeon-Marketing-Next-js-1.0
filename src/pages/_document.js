77dd568e2eda48d426a6eaf380f9a9c797c24e12
Muhammad Wahab Sharif <www.rex19@gmail.com>
2023-08-30 10:47:25 +0500

    tags

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -15,11 +15,11 @@ class MyDocument extends Document {
           <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
           <meta
             name="keywords"
-            content="HTML5 Template Avo onepage themeforest"
+            content="Aeon Marketing - A Real Estate Agency"
           />
           <meta
             name="description"
-            content="Avo - Onepage Multi-Purpose HTML5 Template"
+            content="Aeon Marketing - A Real Estate Agency"
           />
           <meta name="author" content="" />
           <link
