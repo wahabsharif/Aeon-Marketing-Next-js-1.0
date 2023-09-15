830300a5b341d0124879f4de360caa05b0c3ca8e
Muhammad Wahab Sharif <www.rex19@gmail.com>
2023-09-15 19:11:46 +0500

    basic

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "2c76fbc0de20a0e9"; }
+/******/ 		__webpack_require__.h = function() { return "1ee42f45065c51fb"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
